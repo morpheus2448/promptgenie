6e9166368a0f0395555c0a287f499d5662af7a2d
morpheus2448 <41052975+morpheus2448@users.noreply.github.com>
2024-02-14 07:51:44 +0000

    Adding support for glow CLI markdown reader
    Logging generations
    
    General improvements to script

--- pg.sh
+++ pg.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
-#
+
 cd ~/pg
-#
+
 QUERY="$*"
+
 source ./settings.txt
+
+DATE_STAMP=$(date +"%G%V%d%H%M%S")
+HEADER="🧞 $DATE_STAMP - $MODEL_NAME - \"$QUERY\""
+
 #
-echo -n "🧞 -"
-tput bold; \
-$LLAMA_DIR/main -m "$MODEL_DIR/$MODEL_FILE" \
--p "$PROMPT" --prompt-cache "cache.bin" --samplers "min_p;temp" \
---min-p $MIN_P --temp $TEMP --threads $NUM_THREADS \
--e --no-display-prompt --log-disable ; tput sgr0 ; echo
+
+function generate() {
+	$LLAMA_DIR/main -m "$MODEL_DIR/$MODEL_FILE" \
+	-p "$PROMPT" --prompt-cache "cache.bin" --samplers "min_p;temp" \
+	--repeat-penalty $REPEAT_PEN --min-p $MIN_P --temp $TEMP --threads $NUM_THREADS \
+	-c 0 -e --no-penalize-nl --no-display-prompt --log-disable
+}
+
+tput setaf 7;
+echo "# $HEADER" | tee generations/$DATE_STAMP.md
+
+echo | tee -a generations/$DATE_STAMP.md
+generate | tee -a generations/$DATE_STAMP.md
+
+if [ $USE_GLOW == True ]; then
+	glow -p generations/$DATE_STAMP.md
+fi
+
+tput setaf sgr0
+echo
